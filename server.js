d8f75405c7214c5a9301c4a53a2c6d2b785f493b
Tom Holiday <tom.holiday@mac.com>
2023-06-05 17:23:34 -0600

    tired now

--- server.js
+++ server.js
@@ -150,14 +150,14 @@ app.put('/image', (req, res) => {
 //     .catch(err => res.status(400).json('unable to get entries'))
 //   })
 
-// app.listen(process.env.PORT || 3000, ()=> {
-//     console.log(`app is running on port ${process.env.PORT}`); 
-// })
-
-
-app.listen(3000, ()=> {
-    console.log('app is running on port 3000'); 
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`app is running on port ${process.env.PORT}`); 
 })
+
+
+// app.listen(3000, ()=> {
+//     console.log('app is running on port 3000'); 
+// })
 
 
 // // Load hash from your password DB.
